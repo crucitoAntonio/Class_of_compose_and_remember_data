6d8e716b66d0c3896db132e74d1d5066344236fa
Jose Cruz <joseantonio.cruz.lagunas@gmail.com>
2025-01-02 19:25:27 -0600

    Update for play store submit

--- app/src/main/java/com/yourcompany/bullseye/BaseActivity.kt
+++ app/src/main/java/com/yourcompany/bullseye/BaseActivity.kt
@@ -13,17 +13,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
-import com.yourcompany.bullseye.screens.AboutScreen
-import com.yourcompany.bullseye.screens.GameScreen
 import com.yourcompany.bullseye.ui.theme.BullseyeTheme
-import com.yourcompany.bullseye.utils.ConstantsGame
 
 open class BaseActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
